4c6be205b79a2b287da181bf9e928769deffef81
JiWon <radiohead@knou.ac.kr>
2022-09-26 15:00:47 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# testServlet
+ 자바 서블릿 예제
---
+++ src/main/java/ex01/InputServlet2.java
@@ -0,0 +1,56 @@
+package ex01;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InputServlet2
+ */
+@WebServlet("/InputServlet2")
+public class InputServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InputServlet2() {
+       System.out.println("init 메서드 호출");
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("utf-8");
+		Enumeration enu = request.getParameterNames();
+		while (enu.hasMoreElements()) {
+			String name = (String) enu.nextElement();
+			String[] values = request.getParameterValues(name);
+			for (String value : values) {
+				System.out.println("name=" + name + ", value=" + value);
+			}
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	public void destory() {
+		System.out.println("destroy 메서드 호출");
+	}
+
+}
